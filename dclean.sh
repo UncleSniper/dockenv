3e44db11d8df1de73d0e7ea243a3c6daab91c414
Simon "UncleSniper" Bausch <simon@bausch-alfdorf.de>
2018-07-06 10:43:37 +0000

    better dclean.sh

--- dclean.sh
+++ dclean.sh
@@ -2,8 +2,19 @@
 
 set -e
 
+function usedSpace {
+	df --output=used -BM /var/lib/docker | tail -1 | sed 's/M$//'
+}
+
+before="$(usedSpace)"
+
 docker container prune -f
 dangimg="$(docker images -f "dangling=true" -q)"
 if [ -n "$dangimg" ]; then
 	docker rmi $dangimg
 fi
+
+after="$(usedSpace)"
+
+((delta = before - after))
+echo "Freed up $delta MiB."
